a4e0d1849b3f63b5e007d9573fa9f7623620d21b
IlyaGrebenschikov <ilushagr22@gmail.com>
2025-07-22 08:31:32 +0400

    feat: added settings

---
+++ src/core/__init__.py
@@ -0,0 +1,8 @@
+from .settings import ChromeSettings, FirefoxSettings, Settings, load_settings
+
+__all__ = (
+    "ChromeSettings",
+    "FirefoxSettings",
+    "Settings",
+    "load_settings",
+)
