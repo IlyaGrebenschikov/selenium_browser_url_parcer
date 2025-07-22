2ca64d86068fb42bd8068950f7263aacaeb3b684
IlyaGrebenschikov <ilushagr22@gmail.com>
2025-07-22 08:49:29 +0400

    fix: reworked user agents in setup options funcs

--- src/common/drivers/base.py
+++ src/common/drivers/base.py
@@ -1,12 +1,16 @@
 from abc import abstractmethod
-from typing import Protocol, Any
+from typing import Protocol, Any, Optional
 
 class DriverInterface(Protocol):
     def __init__(self, settings: Any) -> None:
         self._settings = settings
 
     @abstractmethod
-    def setup_options(self, **kwargs: Any) -> Any: ...
+    def setup_options(
+        self,
+        user_agents: Optional[tuple[str]] = None,
+        **kwargs: Any
+        ) -> Any: ...
 
     @abstractmethod
     def init_driver(self, **kwargs: Any) -> Any: ...
--- src/common/drivers/firefox.py
+++ src/common/drivers/firefox.py
@@ -1,24 +1,24 @@
 import random
-from typing import Any
+from typing import Any, Optional
 
 from selenium.webdriver import Firefox, FirefoxOptions
 
 from .base import DriverInterface
 from src.core.settings import FirefoxSettings
 
+
 class FirefoxDriver(DriverInterface):
     def __init__(self, settings: FirefoxSettings) -> None:
         super().__init__(settings)
 
-    def setup_options(self, **kwargs: Any) -> FirefoxOptions:
+    def setup_options(
+        self, user_agents: Optional[tuple[str]], **kwargs: Any
+    ) -> FirefoxOptions:
         options = FirefoxOptions(**kwargs)
-        USER_AGENTS = [
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0",
-        ]
-        user_agent = random.choice(USER_AGENTS)
-        options.add_argument(f"user-agent={user_agent}")
+
+        if user_agents:
+            user_agent = random.choice(user_agents)
+            options.add_argument(f"user-agent={user_agent}")
 
         if self._settings.headless:
             options.headless = True
@@ -30,14 +30,9 @@ class FirefoxDriver(DriverInterface):
         return options
 
     def init_driver_firefox(
-        options: FirefoxOptions,
-        extentions: tuple[tuple[str, bool]],
-        **kwargs: Any
+        options: FirefoxOptions, extentions: tuple[tuple[str, bool]], **kwargs: Any
     ) -> Firefox:
-        driver = Firefox(
-            options=options,
-            **kwargs
-            )
+        driver = Firefox(options=options, **kwargs)
 
         for extention in extentions:
             driver.install_addon(*extention)
